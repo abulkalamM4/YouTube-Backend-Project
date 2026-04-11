9aaa83b9e0c59eb7fbe05c68907518370984a481
abulkalam <kvai2077@gmail.com>
2026-04-11 23:15:15 +0600

    Add getWatchHistory endpoint to retrieve user's watch history

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -377,6 +377,61 @@ const getUserChannelProfile = asyncHandler(async(req, res) => {
       new ApiResponse(200, channel[0], "User channel fatched successfully")
    )
 })
+
+const getWatchHistory = asyncHandler(async(req,res) => {
+   const user = await User.aggregate([
+      {
+         $match: {
+            _id: new mongoose.Types.ObjectId(req.user._id)
+         }
+      },
+      {
+         $lookup: {
+            from: "videos",
+            localField: "watchHistory",
+            foreignField: "_id",
+            as: "watchHistory",
+            pipeline: [
+               {
+                  $lookup: {
+                     from: "users",
+                     localField: "owner",
+                     foreignField: "_id",
+                     as: "owner",
+                     pipeline: [
+                        {
+                           $project: {
+                              fullName: 1,
+                              username: 1,
+                              avatar: 1
+                           }
+                        }
+                     ]
+                  }
+               },
+               {
+                  $addFields: {
+                     owner: {
+                        $first: "$owner",
+                        // $arrayElemAt: ["$owner", 0]
+                     }
+                  }
+               }
+            ]
+         }
+      }
+   ])
+
+   return res
+   .status(200)
+   .json(
+      new ApiResponse(
+         200,
+         user[0].watchHistory,
+         "Watch history fetched successfully"
+      )
+   )
+})
    
 export {
    registerUser,
@@ -388,5 +443,6 @@ export {
    updateAccountDetails, 
    updateUserAvatar,
    updateUserCoverImage,
-   getUserChannelProfile
+   getUserChannelProfile,
+   getWatchHistory
  }
